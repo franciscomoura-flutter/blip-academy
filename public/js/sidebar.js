ab0c8fb8fbe89d2b13cdf4bbb8913c0cf01b6532
franciscomoura-flutter <francisco.moura@flutter.com>
2025-10-07 16:09:20 +0100

    Implemented sidebar

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -1,64 +1,64 @@
 export function createSidebar() {
-    const sidebarHTML = `
+  const sidebarHTML = `
     <div class="sidebar">
       <div class="sidebar-section">
-        <a href="/"><img src="/images/logo.png" alt="Logo" class="sidebar-logo"></a>
+        <a href="/"><img src="assets/logo.svg" alt="Logo" class="sidebar-logo"></a>
       </div>
       <div class="sidebar-section">
         <span class="sidebar-title">Start</span>
         <div class="sidebar-link">
-            <div class="sidebar-link-icon"></div>
+            <div class="sidebar-link-icon icon-home"></div>
             <span class="sidebar-link-text">Home</span>
         </div>
         <div class="sidebar-link">
-            <div class="sidebar-link-icon"></div>
+            <div class="sidebar-link-icon icon-about"></div>
             <span class="sidebar-link-text">About Us</span>
         </div>
       </div>
       <div class="sidebar-section">
         <span class="sidebar-title">Learn</span>
         <div class="sidebar-link">
-            <div class="sidebar-link-icon"></div>
+            <div class="sidebar-link-icon icon-onboarding"></div>
             <span class="sidebar-link-text">Onboarding</span>
         </div>
         <div class="sidebar-link">
-            <div class="sidebar-link-icon"></div>
+            <div class="sidebar-link-icon icon-human"></div>
             <span class="sidebar-link-text">Human Skills</span>
         </div>
         <div class="sidebar-link">
-            <div class="sidebar-link-icon"></div>
+            <div class="sidebar-link-icon icon-leadership"></div>
             <span class="sidebar-link-text">Leadership</span>
         </div>
         <div class="sidebar-link">
-            <div class="sidebar-link-icon"></div>
+            <div class="sidebar-link-icon icon-tech"></div>
             <span class="sidebar-link-text">Tech</span>
         </div>
         <div class="sidebar-link">
-            <div class="sidebar-link-icon"></div>
+            <div class="sidebar-link-icon icon-sports"></div>
             <span class="sidebar-link-text">Sports Betting</span>
         </div>
       </div>
       <div class="sidebar-section">
         <span class="sidebar-title">Access</span>
         <div class="sidebar-link">
-            <div class="sidebar-link-icon"></div>
+            <div class="sidebar-link-icon icon-compliance"></div>
             <span class="sidebar-link-text">Compliance</span>
             <div class="sidebar-link-open"></div>
         </div>
         <div class="sidebar-link">
-            <div class="sidebar-link-icon"></div>
+            <div class="sidebar-link-icon icon-sdf"></div>
             <span class="sidebar-link-text">Self-Development Fund</span>
             <div class="sidebar-link-open"></div>
         </div>
         <div class="sidebar-link">
-            <div class="sidebar-link-icon"></div>
+            <div class="sidebar-link-icon icon-tools"></div>
             <span class="sidebar-link-text">Tools & Partnerships</span>
         </div>
       </div>
       <div class="sidebar-section">
         <span class="sidebar-title">Feedback</span>
         <div class="sidebar-link">
-            <div class="sidebar-link-icon"></div>
+            <div class="sidebar-link-icon icon-feedback"></div>
             <span class="sidebar-link-text">Help us grow</span>
             <div class="sidebar-link-open"></div>
         </div>
@@ -66,7 +66,7 @@ export function createSidebar() {
     </div>
   `;
 
-    const sidebarDiv = document.createElement('div');
-    sidebarDiv.innerHTML = sidebarHTML;
-    document.body.insertBefore(sidebarDiv.firstElementChild, document.body.firstChild);
+  const sidebarDiv = document.createElement('div');
+  sidebarDiv.innerHTML = sidebarHTML;
+  document.body.insertBefore(sidebarDiv.firstElementChild, document.body.firstChild);
 }
